e1b8049781ed5627d1745729b51daffa2b31b2dd
Maassas Michael <139464736+MaxVSCJP@users.noreply.github.com>
2026-01-18 14:46:58 +0300

    fix: user model

--- Services/AuthServices.js
+++ Services/AuthServices.js
@@ -75,7 +75,7 @@ export const signupService = async (userData) => {
       password: hashed,
       role: role === VALID_ROLES.admin ? VALID_ROLES.user : role,
       phone,
-      cvImage,
+      cv: cvImage,
     });
 
     const { password, ...safeUser } = newUser;
